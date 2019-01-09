d1e60659d49e1995c8020ee7423f281015155b39
Jongsik Kim <jfreshik@gmail.com>
2019-01-09 15:27:19 +0900

    implement greeting service

---
+++ greeting-service/src/test/java/hello/GreetingControllerTest.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldReturnContainsRequestParamNameValue() {
+
+        final String name = "jfreshik";
+
+        final ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:" + this.port + "/greeting?name=" + name, String.class);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).contains(name);
+    }
+
+}
